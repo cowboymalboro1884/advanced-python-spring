599999ee1da76482a2ec4f3806ad614e6da7dbbb
Даниил Нейков <nejkovdaniil@gmail.com>
2025-03-20 18:18:52 +0000

    clean artifacts

--- hw-2/mygenlatex/bin/generators.py
+++ hw-2/mygenlatex/bin/generators.py
@@ -33,7 +33,7 @@ def generate_document(filename, data):
     with open(filename, "w") as f:
         f.write("\\documentclass{article}\n")
         f.write("\\usepackage[utf8]{inputenc}\n")
-        f.write("\\usepackage{graphicx}")
+        f.write("\\usepackage{graphicx}\n")
         f.write("\\begin{document}\n\n")
         f.write(data)
         f.write("\\end{document}\n")
